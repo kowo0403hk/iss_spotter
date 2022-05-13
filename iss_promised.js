0e082290c179e77314a6f26b0842abb724b47285
Alex Cheung <alexc.sfu@gmail.com>
2022-05-13 05:36:37 +0000

    added index2.js and iss_promised.js. Practiced Promise .then and .catch

---
+++ iss_promised.js
@@ -0,0 +1,27 @@
+const request = require('request-promise-native');
+
+const fetchMyIP = (() => {
+  return request('https://api.ipify.org?format=json')
+});
+
+const fetchCoordsByIP = ((data) => {
+  let ip = JSON.parse(data).ip;
+  return request(`https://freegeoip.app/json/${ip}`)
+});
+
+const fetchISSFlyOverTimes = ((data) => {
+  let {latitude, longitude} = JSON.parse(data);
+  return request(`https://iss-pass.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`);
+});
+
+const nextISSTimesForMyLocation = (() => {
+  return fetchMyIP()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then(data => {
+      const {response} = JSON.parse(data);
+      return response;
+    })
+})
+
+module.exports = {nextISSTimesForMyLocation};
